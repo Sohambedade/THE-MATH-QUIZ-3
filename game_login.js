e2c55c5b36cbc4445ec0a0302a7f9010bdff6290
Sohambedade <97166009+Sohambedade@users.noreply.github.com>
2022-12-05 06:24:32 -0500

    Add files via upload

---
+++ game_login.js
@@ -0,0 +1,12 @@
+
+function addUser()
+ {
+  player1_name = document.getElementById("player1-input").value;
+  player2_name = document.getElementById("player2-input").value;
+
+    localStorage.setItem("player1_name", player1_name);
+    localStorage.setItem("player2_name", player2_name);
+
+    window.location = "game_page.html";
+}
+
